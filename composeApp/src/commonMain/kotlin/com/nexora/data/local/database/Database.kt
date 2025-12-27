8f9c521371595061d639d5af02e53309dc873dbc
leonardorosasilva <03466316022@edu.udesc.br>
2025-12-27 20:42:24 -0300

    create database structure
    create nexora database with entities and daos

---
+++ composeApp/src/commonMain/kotlin/com/nexora/data/local/database/Database.kt
@@ -0,0 +1,26 @@
+package com.nexora.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nexora.data.local.entity.UserEntity
+import com.nexora.data.local.entity.MatchEntity
+import com.nexora.data.local.entity.MeetingEntity
+import com.nexora.data.local.entity.InsightEntity
+import com.nexora.data.local.dao.UserDao
+import com.nexora.data.local.dao.MatchDao
+import com.nexora.data.local.dao.MeetingDao
+import com.nexora.data.local.dao.InsightDao
+import com.nexora.data.local.dao.MessageDao
+
+@Database(
+    entities = [UserEntity::class, MatchEntity::class, MeetingEntity::class, InsightEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class NexoraDatabase : RoomDatabase(){
+    abstract fun userDao(): UserDao
+    abstract fun matchDao(): MatchDao
+    abstract fun meetingDao(): MeetingDao
+    abstract fun insightDao(): InsightDao
+    abstract fun messageDao(): MessageDao
+}
